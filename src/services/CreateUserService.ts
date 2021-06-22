7526e0ec647c3ec67f1a6c6c8c00691f9d2504ae
Caique <caiquepatelliescapeline@gmail.com>
2021-06-22 16:58:46 -0300

    Implementação das regras de usuario
    -Criação da camada services
    -Criação da classe que implementa as regras de usuário

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,37 @@
+import { UsersRepositories } from "../repositories/UsersRepository";
+
+interface IUserRequest{
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserService{
+    async execute({name, email, admin}: IUserRequest){
+        const usersRepository = new UsersRepositories();
+
+        if(!email){ //verifica se o email está preenchido
+            throw new Error("Email incorrect");
+        }
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists){ //verifica se o usuario já existe
+            throw new Error("User already exists");
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin
+        });
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export {CreateUserService};
