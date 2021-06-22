8057b309b78f35dff16c307199b3e68f7b7d95e6
Caique <caiquepatelliescapeline@gmail.com>
2021-06-22 17:28:54 -0300

    Criação das rotas
    -Criação do arquivo de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import "reflect-metadata";      //Banco de dados
-import express from "express";  //Server
+import "reflect-metadata";         //Banco de dados
+import express from "express";     //Server
+import { router } from "./routes"; //Rotas
 
 import "./database"; //Conexão com o banco de dados
 
-//Iniciar o servidor utilizando o express 
-const app = express();
+const app = express(); //Iniciar o servidor utilizando o express 
+
+app.use(express.json()); //Define que os route parms serão do tipo .json
+app.use(router); //Implementar rotas
 
 app.listen(3000, () => console.log("Server is running!")); //inicia o server na porta 3000 -- http://localhost:3000
