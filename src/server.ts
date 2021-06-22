f0b639837d4f510111ee668e195032dc591d7dc2
Caique <caiquepatelliescapeline@gmail.com>
2021-06-22 15:07:09 -0300

    Criação do banco de dados
    -limpeza do código de teste
    -criação do banco de dados SQLite
    -criação da comunicação com o banco de dados com typeorm e reflect-metadata

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,9 @@
+import "reflect-metadata";      //Banco de dados
+import express from "express";  //Server
+
+import "./database"; //Conexão com o banco de dados
+
 //Iniciar o servidor utilizando o express 
-import express from "express";
-
 const app = express();
-
-/** Metodos HTTP
- * GET      => Buscar uma informação
- * POST     => Inserir (Criar) uma informação
- * PUT      => Alterar uma informação
- * DELETE   => Remover um dado
- * PATCH    => Alterar uma informação especifica
- */
-
-//Rotas
-app.get("/test", (request, response) => {
-    //Request  => Entrando
-    //Response => Saindo
-    return response.send("Olá NLW");
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Teste post");
-});
 
 app.listen(3000, () => console.log("Server is running!")); //inicia o server na porta 3000 -- http://localhost:3000
